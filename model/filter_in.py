c27058bfd99d41c17f64941c939a92d756cb9f29
Parth Shah <parthvshah@gmail.com>
2022-12-01 21:22:19 -0800

    New gold files in data

--- model/filter_in.py
+++ model/filter_in.py
@@ -1,5 +1,5 @@
-for_test_raw = open("./data/for_test_raw.txt", "r")
-test_in = open("./data/test_input.txt", "w+")
+for_test_raw = open("../data/gold_output.txt", "r")
+test_in = open("../data/gold_input.txt", "w+")
 
 for line in for_test_raw.readlines():
     tokens = line.split()
